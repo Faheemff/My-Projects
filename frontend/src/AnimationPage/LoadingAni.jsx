90fae9d4fe9d77b043c8083767aa5007d834b21d
Faheemff <mfaheemweb@gmail.com>
2025-01-31 19:00:51 +0500

    add

--- frontend/src/AnimationPage/LoadingAni.jsx
+++ frontend/src/AnimationPage/LoadingAni.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 const LoadingAni = () => {
     return (
         <main className='h-[100vh] w-[100vw] bg-black text-white flex justify-center items-center relative'>
-            <div className="relative text-white w-[32vw] h-[50vh] border-[5px] border-gray-600 rounded-full">
-            <h1 className='text-3xl'>Faheem</h1>
-                <div className="heading text-white text-3xl">
-                    
+            <div className="relative flex flex-col gap-[30px] justify-center items-center text-center text-white w-[25vw] h-[50vh] border-[0.6px] border-gray-600 rounded-full">
+                <div className="heading text-gray-400 text-center h-[50px]">
+                    <h1 className='text-[20px] w-[150px] text-center leading-[20px]'>Wellcome To My Potfolio</h1>
+                </div>
+                <div className="number">
+                    <h1>1</h1>
                 </div>
             </div>
         </main>
